330d1bebbc2c4b7bb5c5a18099376a482e9a1f6d
Paolo Matteucci <itcspm@hants.gov.uk>
2025-01-03 20:06:14 +0000

    Added Headless link provider to Platform.

---
+++ src/platform/LinkProviders/HeadlessLinkProvider.cs
@@ -0,0 +1,31 @@
+﻿namespace dotnetcore_xp_sxa_demo.LinkProviders
+{
+    using System;
+
+    using Sitecore.Abstractions;
+    using Sitecore.Data.Items;
+    using Sitecore.Links.UrlBuilders;
+
+    public class HeadlessLinkProvider : Sitecore.Links.LinkProvider
+    {
+        public HeadlessLinkProvider(BaseFactory factory) : base(factory)
+        {
+        }
+
+        public override string GetItemUrl(Item item, ItemUrlBuilderOptions options)
+        {
+            var url = base.GetItemUrl(item, options);
+            if (!VirtualPathIsNeeded(options, url))
+            {
+                return url;
+            }
+
+            return $"/headlessdemo{url}";
+        }
+
+        private static bool VirtualPathIsNeeded(ItemUrlBuilderOptions options, string url)
+        {
+            return options?.Site?.Name == "hcc-demo-site-3-embedded" && Uri.IsWellFormedUriString(url, UriKind.Relative);
+        }
+    }
+}
